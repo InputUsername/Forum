854b5e76c17c50a1cb144db761e39668ca2314f7
Koen Bolhuis <koen.bolhuis@gmail.com>
2016-04-03 16:42:23 +0200

    Fixed templates

--- src/includes/templates.php
+++ src/includes/templates.php
@@ -18,10 +18,10 @@ class TemplateException extends \Exception {}
 
 class Template {
 	// Template includes regex
-	private final static $INC_REGEX = '/\{\{\s*include "(.+)"\s*\}\}/';
+	private static $INC_REGEX = '/\{\{\s*include "(.+)"\s*\}\}/';
 
 	// Template variable regex
-	private final static $VAR_REGEX = '/\{\{\s*([A-Za-z_][A-Za-z0-9_]*)\s*\}\}/';
+	private static $VAR_REGEX = '/\{\{\s*([A-Za-z_][A-Za-z0-9_]*)\s*\}\}/';
 
 	// Main template file
 	private $file;
@@ -83,34 +83,37 @@ class Template {
 	Prepare the template by substituting bound variables in the template file
 	*/
 	public function prepare() {
-		if (!file_exists($file)) {
-			throw new TemplateException('File ' + $file + ' does not exist');
+		if (!file_exists($this->file)) {
+			throw new TemplateException('File ' + $this->file + ' does not exist');
 		}
 
-		$content = file_get_contents($file);
+		$content = file_get_contents($this->file);
 
 		/*
 		Include files
 		*/
-		$prepared = preg_replace(Template::$INC_REGEX, function($matches) {
-			$path = joinPaths(basename($file), $matches[0]);
+		$prepared = preg_replace_callback(Template::$INC_REGEX, function($matches) {
+			$path = joinPaths(dirname($this->file), $matches[1]);
 			if (!file_exists($path)) {
 				return '<no such file ' . $path . '>';
 			}
-			return new Template($path)->prepare()->output();
+			return (new Template($path))->prepare()->output();
 		}, $content);
 
 		if ($prepared === NULL) {
 			throw new TemplateException('There was an error preparing the template');
 		}
 
+		/*
+		Replace variables
+		*/
 		if (count($this->templateValues) > 0) {
 			/*
 			Prepare the template by replacing {{variables}} by the values bound
 			to the same name in the template instance
 			*/
-			$prepared = preg_replace(Template::$VAR_REGEX, function($matches) {
-				$name = $matches[0];
+			$prepared = preg_replace_callback(Template::$VAR_REGEX, function($matches) {
+				$name = $matches[1];
 				if (!isset($this->templateValues[$name])) {
 					return '<no template variable ' . $name . '>';
 				}
