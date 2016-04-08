355133636fb1d78a0ae1e8285c86ccd5ae992a87
Koen Bolhuis <koen.bolhuis@gmail.com>
2016-04-08 14:46:32 +0200

    -

--- src/includes/smarty_setup.php
+++ src/includes/smarty_setup.php
@@ -4,19 +4,13 @@ namespace forum;
 
 require_once($config['smarty_dir'] . 'Smarty.class.php');
 
-$dirs = array('templates_c', 'cache', 'configs');
-foreach ($dirs as $dirname) {
-	$dir = 'includes/smarty_dirs/' . $dirname;
-	if (!is_dir($dir)) {
-		mkdir($dir);
-	}
+if (!is_dir('includes/templates_c')) {
+	mkdir('includes/templates_c');
 }
 
 $smarty = new \Smarty();
 
 $smarty->setTemplateDir('includes/templates/');
-$smarty->setCompileDir('includes/smarty_dirs/templates_c/');
-$smarty->setCacheDir('includes/smarty_dirs/cache/');
-$smarty->setConfigDir('includes/smarty_dirs/configs/');
+$smarty->setCompileDir('includes/templates_c/');
 
 ?>
