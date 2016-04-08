3f0fe52d1aabbda5ec1dcc7d121cb5dd80e96497
Koen Bolhuis <koen.bolhuis@gmail.com>
2016-04-08 16:37:53 +0200

    Update user page

--- src/includes/templates/user.tpl
+++ src/includes/templates/user.tpl
@@ -2,5 +2,15 @@
 {block name="head" prepend}
 {/block}
 {block name=content}
-This is the user page
+<div id="userInfo">
+	{assign var="profilePicture" value="assets/uploads/profile/{$user->profilePicture}"}
+	{if !file_exists($profilePicture)}
+		{assign var="profilePicture" value="custom_assets/profile_default.png"}
+	{/if}
+	<img id="userImage" src="/{$profilePicture}" alt="{$user->username}">
+	<h1 id="userName">{$user->username}</h1>
+	{if $user->realName != ""}
+		<h2 id="userRealName">{$user->realName}</h2>
+	{/if}
+</div>
 {/block}
--- src/user.php
+++ src/user.php
@@ -2,10 +2,47 @@
 
 namespace forum;
 
+if (!isset($_GET['id'])) {
+	die('incorrect params');
+}
+
 require_once('includes/config.php');
+require_once('includes/database.class.php');
+require_once('includes/user.class.php');
+
+$db = new Database();
+$db->connect(
+	$config['mysql']['host'],
+	$config['mysql']['user'],
+	$config['mysql']['pass'],
+	$config['mysql']['db']
+);
+
+$params = array('i' => $_GET['id']);
+
+$stmt = $db->prepare('SELECT * FROM users WHERE id=? LIMIT 1', $params);
+$result = $db->executePrepared($stmt);
+
+if (!$result) {
+	die('can\'t retrieve user');
+}
+
+$row = $result->fetch_assoc();
+
+$user = new User(
+	$row['id'],
+	$row['username'],
+	$row['email'],
+	$row['real_name'],
+	$row['registered'],
+	$row['last_active'],
+	$row['is_admin']
+);
+
 require_once('includes/smarty_setup.php');
 
-$smarty->assign('pageTitle', 'Index');
+$smarty->assign('pageTitle', 'User profile: ' . $user->username);
+$smarty->assign('user', $user);
 
 $smarty->display('user.tpl');
 
