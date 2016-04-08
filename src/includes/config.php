b60fd100afefc56446d92fe6f232ba5f41e983aa
Koen Bolhuis <koen.bolhuis@gmail.com>
2016-04-08 11:29:49 +0200

    Me too thanks

--- src/includes/config.php
+++ src/includes/config.php
@@ -4,10 +4,14 @@ namespace forum;
 
 $config = array();
 
-$config['profile_upload_dir'] = '/assets/uploads/profile/';
+// Upload directory for profile images
+$config['profile_upload_dir'] = 'assets/uploads/profile/';
 
 $config['mysql'] = array();
 $config['mysql']['host'] = 'localhost';
 $config['mysql']['user'] = 'root';
 $config['mysql']['pass'] = '';
 $config['mysql']['db'] = 'forum';
+
+// Smarty include dir, relative to root
+$config['smarty_dir'] = 'includes/smarty-3.1.29/libs/';
