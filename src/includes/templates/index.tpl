1a2c0c8233c78fa2e50fc540209eb634e5c672d9
Koen Bolhuis <koen.bolhuis@gmail.com>
2016-04-10 18:55:11 +0200

    List top-level subforums on index

--- src/includes/templates/index.tpl
+++ src/includes/templates/index.tpl
@@ -1,6 +1,15 @@
 {extends file="main.tpl"}
+
 {block name="head" prepend}
 {/block}
+
 {block name="content"}
-This is the index page
+	<h1>Subforums</h1>
+	{foreach from=$subforums key="index" item="forum"}
+		<div class="subforumBox">
+			<h2><a href="">{$forum.name}</a></h2>
+		</p>
+	{foreachelse}
+		<p>This forum does not currently have any subforums.</p>
+	{/foreach}
 {/block}
--- src/index.php
+++ src/index.php
@@ -4,8 +4,45 @@ namespace forum;
 
 require_once('includes/config.php');
 require_once('includes/smarty_setup.php');
+require_once('includes/classes/database.class.php');
+require_once('includes/classes/user.class.php');
+
+// Connect to database
+
+$db = new Database();
+try {
+	$db->connect(
+		$config['mysql']['host'],
+		$config['mysql']['user'],
+		$config['mysql']['pass'],
+		$config['mysql']['db']
+	);
+}
+catch (DatabaseException $e) {
+	$smarty->assign('pageTitle', 'Database error');
+	$smarty->display('errors/database_error.tpl');
+	die();
+}
+
+// Query database
+
+$result = $db->query('SELECT * FROM subforums WHERE parent IS NULL');
+
+// Error or something
+if (!$result) {
+	//TODO: error
+	die();
+}
+
+$subforums = array();
+
+while ($row = $result->fetch_assoc()) {
+	$subforums[] = $row;
+}
 
 $smarty->assign('pageTitle', 'Index');
+
+$smarty->assign('subforums', $subforums);
 
 $smarty->display('index.tpl');
 
