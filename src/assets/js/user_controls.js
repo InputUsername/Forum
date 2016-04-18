498bba3f0452dcadf14be1c72669a4c63a2f2b16
Koen Bolhuis <koen.bolhuis@gmail.com>
2016-04-18 14:49:38 +0200

    Improved menu toggle

--- src/assets/js/user_controls.js
+++ src/assets/js/user_controls.js
@@ -3,10 +3,28 @@
 
 	var $menuToggle = getById('userControlsToggle'),
 		$menu = getById('userControlsMenu');
-
+	
+	var toggleMenu = function(state) {
+		$menu.classList.toggle('open', state);
+	};
+	
 	$menuToggle.addEventListener('click', function(event) {
+		event = event || window.event;
 		event.preventDefault();
-
-		$menu.classList.toggle('open', !$menu.classList.contains('open'));
+	});
+	
+	document.addEventListener('click', function(event) {
+		event = event || window.event;
+		
+		var target = event.target || event.srcElement;
+		
+		if (target.id === 'userControlsImage' || target.id === 'userControlsToggle') {
+			event.preventDefault();
+			
+			toggleMenu(!$menu.classList.contains('open'));
+		}
+		else if (target.id !== 'userControlsMenu') {
+			toggleMenu(false);
+		}
 	});
 })();
